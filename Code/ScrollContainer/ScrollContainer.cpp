bd302a09b95a4bf3855f5a56b5a714f46fcad565
Björn Sundin <bjornsundin02@gmail.com>
2019-10-24 22:11:51 +0200

    Work on ScrollContainer

--- Code/ScrollContainer/ScrollContainer.cpp
+++ Code/ScrollContainer/ScrollContainer.cpp
@@ -5,11 +5,40 @@
 float const SCROLLBAR_OPACITY_NORMAL = 0.3f;
 float const SCROLLBAR_OPACITY_HOVERING = 0.6f;
 
+float const SCROLLBAR_WIDTH = 1 * 8.f;
+
+//------------------------------
+
+Scrollbar::Scrollbar(ScrollContainer* p_parent, Orientation p_orientation) :
+	View(p_parent), m_isDragging(false), m_opacity(SCROLLBAR_OPACITY_NORMAL)
+{
+	setCornerRadius(3.f);
+
+	setCursor(AvoGUI::Cursor::Hand);
+}
+
+void Scrollbar::updateAnimations()
+{
+	if (m_isHovering)
+	{
+		if (m_opacityAnimationTime < 1.f)
+		{
+			m_opacity = AvoGUI::interpolate(SCROLLBAR_OPACITY_NORMAL, SCROLLBAR_OPACITY_HOVERING, getThemeEasing("in out").easeValue(m_opacityAnimationTime += 0.1f));
+		}
+	}
+	else
+	{
+		if (m_opacityAnimationTime > 0.f)
+		{
+			m_opacity = AvoGUI::interpolate(SCROLLBAR_OPACITY_NORMAL, SCROLLBAR_OPACITY_HOVERING, getThemeEasing("in out").easeValue(m_opacityAnimationTime -= 0.1f));
+		}
+	}
+}
+
 //------------------------------
 
 ScrollContainer::ScrollContainer(AvoGUI::View* p_parent, AvoGUI::Rectangle<float> const& p_bounds) :
-	AvoGUI::View(p_parent, p_bounds),
-	m_scrollbarOpacity()
+	AvoGUI::View(p_parent, p_bounds)
 {
 	enableMouseEvents();
 }
--- Code/ScrollContainer/ScrollContainer.hpp
+++ Code/ScrollContainer/ScrollContainer.hpp
@@ -1,6 +1,65 @@
 #pragma once
 
 #include <AvoGUI.hpp>
+#include "../../Resources/Colors.hpp"
+
+//------------------------------
+
+class ScrollContainer;
+
+class Scrollbar :
+	public AvoGUI::View
+{
+public:
+	enum class Orientation
+	{
+		Horizontal,
+		Vertical
+	};
+
+private:
+	ScrollContainer* m_scrollContainer;
+
+	Orientation m_orientation;
+	bool m_isDragging;
+
+	bool m_isHovering;
+	float m_opacityAnimationTime;
+	float m_opacity;
+
+public:
+	Scrollbar(ScrollContainer* p_parent, Orientation p_orientation);
+
+	//------------------------------
+
+	void handleMouseEnter(AvoGUI::MouseEvent const& p_event) override
+	{
+		m_isHovering = true;
+		m_opacityAnimationTime = 0.f;
+		queueAnimationUpdate();
+	}
+	void handleMouseLeave(AvoGUI::MouseEvent const& p_event) override
+	{
+		m_isHovering = false;
+		m_opacityAnimationTime = 1.f;
+	}
+	void handleMouseDown(AvoGUI::MouseEvent const& p_event) override
+	{
+		m_isDragging = true;
+	}
+	void handleMouseUp(AvoGUI::MouseEvent const& p_event) override;
+	void handleMouseMove(AvoGUI::MouseEvent const& p_event) override;
+
+	//------------------------------
+
+	void updateAnimations() override;
+
+	void draw(AvoGUI::DrawingContext* p_context) override
+	{
+		p_context->setColor(AvoGUI::Color(Colors::scrollbar, m_opacity));
+		p_context->fillRectangle(getSize());
+	}
+};
 
 //------------------------------
 
@@ -12,49 +71,75 @@ private:
 	AvoGUI::Point<float> m_contentSize;
 	AvoGUI::Point<float> m_scrollPosition;
 
-	AvoGUI::Rectangle<float> m_scrollbarBounds;
-	float m_scrollbarOpacity;
+	Scrollbar* m_horizontalScrollbar;
+	Scrollbar* m_verticalScrollbar;
 
 public:
 	ScrollContainer(AvoGUI::View* p_parent, AvoGUI::Rectangle<float> const& p_bounds = AvoGUI::Rectangle<float>());
 
 	//------------------------------
 
-	void handleChildAttachment(View* p_child) override
+	void handleMouseScroll(AvoGUI::MouseEvent const& p_event) override;
+
+	//------------------------------
+
+	void handleChildAttachment(AvoGUI::View* p_child) override
 	{
-		if (p_child->getRight() > m_contentSize.x)
+		if (p_child != m_horizontalScrollbar && p_child != m_verticalScrollbar)
 		{
-			m_contentSize.x = p_child->getRight();
-		}
-		if (p_child->getBottom() > m_contentSize.y)
-		{
-			m_contentSize.y = p_child->getBottom();
+			if (p_child->getRight() > m_contentSize.x)
+			{
+				m_contentSize.x = p_child->getRight();
+			}
+			if (p_child->getBottom() > m_contentSize.y)
+			{
+				m_contentSize.y = p_child->getBottom();
+			}
 		}
 	}
-	void handleChildDetachment(View* p_child) override
+	void handleChildDetachment(AvoGUI::View* p_child) override
 	{
-		if (p_child->getRight() == m_contentSize.x)
+		if (p_child != m_horizontalScrollbar && p_child != m_verticalScrollbar)
 		{
-			m_contentSize.x = 0.f;
-			for (auto child : getChildren())
+			if (p_child->getRight() == m_contentSize.x)
 			{
-				if (child->getRight() > m_contentSize.x)
-				{
-					m_contentSize.x = child->getRight();
-				}
+				m_contentSize.x = calculateContentRight();
+			}
+			if (p_child->getBottom() == m_contentSize.y)
+			{
+				m_contentSize.y = calculateContentBottom();
 			}
 		}
-		if (p_child->getBottom() == m_contentSize.y)
+	}
+	void handleViewBoundsChange(AvoGUI::View* p_view, AvoGUI::Rectangle<float> const& p_previousBounds) override
+	{
+		if (p_view == m_horizontalScrollbar || p_view == m_verticalScrollbar)
 		{
-			m_contentSize.y = 0.f;
-			for (auto child : getChildren())
+
+		}
+		else
+		{
+			if (p_view->getRight() > m_contentSize.x)
 			{
-				if (child->getRight() > m_contentSize.y)
-				{
-					m_contentSize.y = child->getRight();
-				}
+				m_contentSize.x = p_view->getRight();
+			}
+			else if (p_previousBounds.right == m_contentSize.x && p_view->getRight() < m_contentSize.x)
+			{
+				m_contentSize.x = calculateContentRight();
+			}
+			if (p_view->getBottom() > m_contentSize.y)
+			{
+				m_contentSize.y = p_view->getBottom();
+			}
+			else if (p_previousBounds.bottom == m_contentSize.y && p_view->getBottom() < m_contentSize.y)
+			{
+				m_contentSize.y = calculateContentBottom();
 			}
 		}
+	}
+	void handleSizeChange() override
+	{
+		invalidate();
 	}
 
 	void drawOverlay(AvoGUI::DrawingContext* p_context)
